670b946a58ccb127e0f3c7a630402cb1cace912a
GopiAjt <gopiajt23@gmail.com>
2023-12-15 21:02:14 +0530

    addeding reset password to photographer

--- photographer-profile/updatedetails.js
+++ photographer-profile/updatedetails.js
@@ -1,7 +1,6 @@
 const updateDetails = document.getElementById('updateDetails');
 
-async function details()
-{
+async function details() {
     let name = document.getElementById('name').value;
     let email = data.email;
     let phoneNumber = document.getElementById('phoneNumber').value;
@@ -32,8 +31,7 @@ async function details()
         })
     })
     console.log(response);
-    if(response.status == 200)
-    {
+    if (response.status == 200) {
         data.name = name;
         data.phoneNumber = phoneNumber;
         data.serviceLocation = serviceLocation;
@@ -43,11 +41,88 @@ async function details()
         data.aboutMe = description;
 
         localStorage.setItem('data', JSON.stringify(data));
-        window.alert('updated successfully!');        
+        window.alert('updated successfully!');
     }
-    if(response.status == 409){
+    if (response.status == 409) {
         window.alert('error');
     }
 }
 
 updateDetails.addEventListener('click', details);
+
+
+// Event listener for "Send OTP" button
+const sendOtpButton = document.getElementById('sendOtp');
+sendOtpButton.addEventListener('click', function () {
+    // Implement logic to send OTP via emails
+    // You can use fetch or any other method here
+    console.log('Sending OTP...');
+});
+
+// Event listener for "Update" button
+const updatePasswordButton = document.getElementById('updatePassword');
+updatePasswordButton.addEventListener('click', function () {
+
+    const oldPasswordInput = document.getElementById('oldPassword');
+    const newPassword1Input = document.getElementById('newPassword1');
+    const newPassword2Input = document.getElementById('newPassword2');
+    const otpInput = document.getElementById('otp');
+    // Validate form inputs
+    if (!validateForm()) {
+        return;
+    }
+
+    // Fetch API to reset password
+    const resetData = {
+        oldPassword: oldPasswordInput.value,
+        newPassword: newPassword1Input.value,
+        otp: otpInput.value,
+    };
+
+    fetch('your_reset_password_api_endpoint', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(resetData),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to reset password');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Password reset successful, handle accordingly (e.g., show success message)
+            console.log('Password reset successful', data);
+        })
+        .catch(error => {
+            // Handle errors (e.g., show error message)
+            console.error('Error resetting password', error);
+        });
+});
+
+// Function to validate form inputs
+function validateForm() {
+    const oldPassword = document.getElementById('oldPassword').value;
+    const newPassword1 = document.getElementById('newPassword1').value;
+    const newPassword2 = document.getElementById('newPassword2').value;
+    const otp = document.getElementById('otp').value;
+
+    // Check if any field is empty
+    if (!oldPassword || !newPassword1 || !newPassword2 || !otp) {
+        alert('Please fill in all fields.');
+        return false;
+    }
+
+    // Check if the new passwords match
+    if (newPassword1 !== newPassword2) {
+        alert('New passwords do not match.');
+        return false;
+    }
+
+    // Add more validation logic as needed
+
+    // If all validation passes, return true
+    return true;
+}
